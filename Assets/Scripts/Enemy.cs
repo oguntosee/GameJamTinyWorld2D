908bd36231a6a8571bef97f35ff4a9e00d5efaf3
Ogün Güngör <oogungor@gmail.com>
2025-05-25 20:15:34 +0300

    commit

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,43 +9,40 @@ public class Enemy : MonoBehaviour
     [Header("References")]
     private Rigidbody2D rb;
     private Animator anim;
+    public SpriteRenderer spriteRenderer;
     private bool facingRight = true;
+
     [Header("Collision Check")]
     [SerializeField] private LayerMask whatIsGround; // ground
     [SerializeField] private Transform groundCheck;
     [SerializeField] private float groundCheckRadius;
     [SerializeField] private bool isGrounded;
+
     [Header("Movement")]
-    
     [SerializeField] private bool isAlive;
-    [SerializeField] public float moveSpeed = 5.0f;
+    [SerializeField] public float moveSpeed = 2.0f;
     [SerializeField] public int health = 3;
     private bool Alive;
-
-
     private Transform playerTransform;
-    private int directionMultiplier;
-
+    private int directionMultiplier = 1; // Baþlangýç deðeri
+    private float baseMoveSpeed; // Orijinal hýz deðerini saklamak için
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>(); //anim
+        anim = GetComponent<Animator>();
+        //anim
         Alive = true;
-
+        baseMoveSpeed = moveSpeed; // Orijinal hýz deðerini sakla
     }
-    
-
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Snowball"))
         {
-
             TakeDamage(1);
             Destroy(collision.gameObject);
-
         }
     }
 
@@ -55,16 +52,15 @@ public class Enemy : MonoBehaviour
         health -= damage;
         if (health <= 0)
         {
-            
             Die();
+            EnemySpawner.Instance.EnemyDied();
         }
     }
 
-
     private void Die()
     {
+        EnemySpawner.Instance.SpawnEntchantMaterial(this.transform.position);
         EnemyDie();
-        
     }
 
     private void UpdateMoveSpeed()
@@ -72,25 +68,36 @@ public class Enemy : MonoBehaviour
         playerTransform = EnemySpawner.Instance.GetPlayerTransform();
         float xPlayer = playerTransform.position.x;
         float xEnemy = this.transform.position.x;
+
         if (xPlayer > xEnemy)
+        {
+            if (spriteRenderer.flipX == true)
+            {
+                spriteRenderer.flipX = false;
+            }
             directionMultiplier = 1;
+        }
         else
+        {
+
+            if (spriteRenderer.flipX == false)
+            {
+                spriteRenderer.flipX = true;
+            }
             directionMultiplier = -1;
+        }
 
-        moveSpeed *= directionMultiplier;
-
+        moveSpeed = baseMoveSpeed * directionMultiplier;
     }
 
     // Update is called once per frame
     void Update()
     {
-
         if (Alive)
         {
             UpdateMoveSpeed();
-            rb.linearVelocity = new Vector2(moveSpeed, rb.linearVelocity.y); //rigidbody
+            rb.linearVelocity = new Vector2(moveSpeed, rb.linearVelocity.y);
         }
-
     }
 
     private void EnemyDie()
@@ -98,22 +105,12 @@ public class Enemy : MonoBehaviour
         StartCoroutine(EnemyDieCoroutine());
     }
 
-
     private IEnumerator EnemyDieCoroutine()
     {
         Alive = false;
         anim.SetTrigger("die");
         GetComponent<Collider2D>().enabled = false;
         yield return new WaitForSeconds(1.5f); // adjust to your animation time
-        
         Destroy(gameObject);
     }
-    /* 
-     anim.SetBool("isAlive", isAlive);
-        
-        GetComponent<Collider2D>().enabled = false;
-        Destroy(gameObject, 0.5f);
-    */
-
-
 }
--- Assets/Scripts/GameSceneManager.cs
+++ Assets/Scripts/GameSceneManager.cs
@@ -4,13 +4,15 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
 
 
 public class GameSceneManager : MonoBehaviour
 {
 
     public static GameSceneManager Instance;
-
+    private bool isEnemySpawnerCreated;
+    //private bool isEntchantSpawnerCreated;
     //
     private Player playerInstance;
 
@@ -28,23 +30,23 @@ public class GameSceneManager : MonoBehaviour
 
 
     //
-    public Dictionary<InventoryItemInfo, int> PlayerInventory;
+    public Dictionary<ItemType, int> PlayerInventory;
 
-    public string activeSceneName;
+    private string activeSceneName;
 
 
 
 
     private void Awake()
     {
-        if(Instance == null)
+        if (Instance == null)
         {
             Instance = this;
             chestStates = new Dictionary<string, ChestState>();
-            PlayerInventory = new Dictionary<InventoryItemInfo, int>();
+            PlayerInventory = new Dictionary<ItemType, int>();
             SceneManager.sceneLoaded += OnSceneLoaded;
             DontDestroyOnLoad(gameObject);
-
+            Debug.Log("GameSceneManager Awake.");
         }
         else
         {
@@ -65,12 +67,17 @@ public class GameSceneManager : MonoBehaviour
 
     private void OnDestroy()
     {
-        if(Instance == this)
+        if (Instance == this)
         {
             SceneManager.sceneLoaded -= OnSceneLoaded;
-        }        
+        }
     }
 
+    public void SetEnemySpawnerCreated(bool isCreated)
+    {
+        isEnemySpawnerCreated = isCreated;
+        NotifyEnemyCanSpawn(activeSceneName);
+    }
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
@@ -80,6 +87,13 @@ public class GameSceneManager : MonoBehaviour
     public void PrepareScene()
     {
         activeSceneName = SceneManager.GetActiveScene().name;
+        Debug.Log("PrepareScene");
+        if (isEnemySpawnerCreated)
+        {
+            Debug.Log("PrepareScene, EnemySpawnerCreated");
+            NotifyEnemyCanSpawn(activeSceneName);
+            NotifyPlayerIsReady(playerInstance);
+        }
 
     }
 
@@ -87,7 +101,7 @@ public class GameSceneManager : MonoBehaviour
     public void AssignPlayer(Player player_instance)
     {
         playerInstance = player_instance;
-        NotifyPlayerIsReady(playerInstance);
+        //NotifyPlayerIsReady(playerInstance);
     }
 
     public void NotifyPlayerIsReady(Player player_instance)
@@ -95,9 +109,11 @@ public class GameSceneManager : MonoBehaviour
         EnemySpawner.Instance.SetPlayerInstance(player_instance);
     }
 
-    public void NotifyEnemyCanSpawn()
+    public void NotifyEnemyCanSpawn(string activeSceneName)
     {
-        EnemySpawner.Instance.SetEnemyCanSpawn(true);
+        Debug.Log("NotifyEnemyCanSpawn()");
+        EnemySpawner.Instance.SetPlayerInstance(playerInstance);
+        EnemySpawner.Instance.SetEnemyCanSpawn(true, activeSceneName);
     }
 
 
@@ -106,26 +122,28 @@ public class GameSceneManager : MonoBehaviour
         return playerInstance;
     }
 
-    public void LootItem(InventoryItemInfo inventoryItemInfo, int lootedAmount)
+    public void LootItem(ItemType itemType, int lootedAmount)
     {
-        if (PlayerInventory.ContainsKey(inventoryItemInfo))
+        if (PlayerInventory.ContainsKey(itemType))
         {
-            PlayerInventory[inventoryItemInfo] += lootedAmount;
+            PlayerInventory[itemType] += lootedAmount;
         }
         else
-        { 
-            PlayerInventory[inventoryItemInfo] = lootedAmount;
+        {
+            PlayerInventory[itemType] = lootedAmount;
         }
-
+        Debug.Log(PlayerInventory[itemType]);
     }
+
+
 
 
     private void FillWizardStoryDialogues()
     {
         List<DialogueLine> wizardStoryDialogue_1 = new List<DialogueLine>();
-        wizardStoryDialogue_1.Add( new DialogueLine("Büyücü : \r\nHoþ geldin… Kirella.\r\nYýllar sonra, bu kürede yeniden bir kýpýrtý hissediyorum.\r\nBelki... bu kez baþarý mümkün olabilir.", SpeakerType.Wizard));
+        wizardStoryDialogue_1.Add(new DialogueLine("Büyücü : \r\nHoþ geldin… Kirella.\r\nYýllar sonra, bu kürede yeniden bir kýpýrtý hissediyorum.\r\nBelki... bu kez baþarý mümkün olabilir.", SpeakerType.Wizard));
         wizardStoryDialogue_1.Add(new DialogueLine("Kirella : \r\nSiz... kimsiniz? Beni nereden tanýyorsunuz?", SpeakerType.Player));
-        wizardStoryDialogue_1.Add( new DialogueLine("Büyücü : \r\nBen... bu kürenin bilgeliðini koruyan oyuncaklardan biriyim.\r\nEskiden birçoklarý özgürlüðü aradý. Ama çoðu... yoldan saptý.\r\n"
+        wizardStoryDialogue_1.Add(new DialogueLine("Büyücü : \r\nBen... bu kürenin bilgeliðini koruyan oyuncaklardan biriyim.\r\nEskiden birçoklarý özgürlüðü aradý. Ama çoðu... yoldan saptý.\r\n"
             + "Þimdi sýra sende. Küreden çýkmak istiyorsun. Ama önce... bedeninle ruhunu birleþtirmen gerek. Hazýr mýsýn?", SpeakerType.Wizard));
         wizardStoryDialogue_1.Add(new DialogueLine("Kirella : Hazýrým!", SpeakerType.Player));
         wizardStoryDialogues.Add(wizardStoryDialogue_1);
@@ -152,13 +170,13 @@ public class GameSceneManager : MonoBehaviour
     public List<DialogueLine> GetDialogueLines(SpeakerType speakerType)
     {
         List<DialogueLine> dialogueLinesToSend = new List<DialogueLine>();
-        if(speakerType == SpeakerType.Wizard)
+        if (speakerType == SpeakerType.Wizard)
         {
             // 1. Get Wizard Story Dialogue Index.
             // 2. Get that DialogueLines List
             // 3. Send it back.
 
-            if(wizardStoryDialogues.Count == wizardStoryDialogueIndex)
+            if (wizardStoryDialogues.Count == wizardStoryDialogueIndex)
             {
                 return (GetRandomDialogueLines(speakerType));
             }
@@ -189,7 +207,7 @@ public class GameSceneManager : MonoBehaviour
 
     public void UpdateChestState(string chest_id, ChestState chestState)
     {
-        if(IsChestAlreadyAssigned(chest_id) == true)
+        if (IsChestAlreadyAssigned(chest_id) == true)
         {
             chestStates[chest_id] = chestState;
             //Debug.Log(chest_id + " : " + chestStates[chest_id].isAlreadyLooted);
@@ -207,8 +225,5 @@ public class GameSceneManager : MonoBehaviour
         transitionDirection = edgeDirection;
     }
 
-
-    
-    
 
 }
